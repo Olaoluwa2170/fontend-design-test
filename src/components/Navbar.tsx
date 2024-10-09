67b958f2fb0ecddcd516b0a32bf831358287699f
Olaoluwa2170 <elitebabson@gmail.com>
2024-10-09 14:08:44 +0100

    Refactor Navbar component and add mobile menu functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/endeavor.png"; // Ensure this path is correct and the file exists
 import { ChevronDown } from "lucide-react";
 import { cn } from "../lib/utils";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false)
+
   return (
     <nav className="md:min-h-[5rem] w-full bg-primary-100 border-b border-white">
       <div className="flex justify-between items-center">
-        <div className="flex items-center bg-black">
-          <div className="md:border-r px-8 py-2 border-white w-fit">
-            <img src={logo} alt="logo" className="w-[50px] md:w-[80px] object-contain" />
+        <div className="flex items-center w-full md:w-fit">
+          <div className="md:border-r md:px-8 py-2 border-white w-fit">
+            <img src={logo} alt="logo" className="w-[80px] object-contain" />
           </div>
-          <div className="md:hidden">
-              <div className="h-[0.0625rem] w-5 bg-black"></div>
+          <div onClick={() => setClick(!click)} className={cn("md:hidden cursor-pointer flex flex-col items-end w-full pr-3", {
+            'gap-2' : !click
+          })}>
+              <div className={cn("h-[2px] rounded-3xl w-5 bg-white duration-300 rotate-45", {
+                'rotate-0 pr-8' : !click
+              })}></div>
+              <div className={cn("h-[2px] rounded-3xl w-5 bg-white duration-300 -rotate-45", {
+                'rotate-0' : !click
+              })}></div>
           </div>
           <div>
             <ul className="md:flex hidden pl-20 font-jost text-white font-semibold text-lg">
