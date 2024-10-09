af19689805064e562895c1de71ccf11eb797727a
Olaoluwa2170 <elitebabson@gmail.com>
2024-10-09 11:54:54 +0100

    Refactor ESLint configuration and added Lucide and Prettier dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 function App() {
-
   return (
     <>
       <h1 className="text-3xl font-bold text-red-300 underline">
-      Hello world!
-    </h1>
+        Hello world!
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
